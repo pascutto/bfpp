fd0e33ffc9a76f76cb4ef8eb7c3c3da8f67528a2
DUSMANU Mihai <mihai.dusmanu@yahoo.com>
2016-01-22 22:47:01 +0100

    run_clock script now delets created files after execution.

--- run_clock.sh
+++ run_clock.sh
@@ -2,3 +2,5 @@
 
 ./create_rom.sh clock/clock_realtime.bfpp
 ./sim/netlist_sim.byte -mem proc/rom.mem -steps $1 -net proc/proc.net | python3 printer.py
+rm -f proc/rom.mem
+rm -f clock/clock_realtime.bin
