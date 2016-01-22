1ade473a287728083b064d37e6c578ad91933396
DUSMANU Mihai <mihai.dusmanu@yahoo.com>
2016-01-23 00:26:37 +0100

    Added a reasonably sexy printer.

--- run_clock.sh
+++ run_clock.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./create_rom.sh clock/clock_realtime.bfpp
-./sim/netlist_sim.byte -mem proc/rom.mem -steps $1 -net proc/proc.net | python3 printer.py
+./sim/netlist_sim.byte -mem proc/rom.mem -steps $1 -net proc/proc.net | python3 sexy_printer.py
