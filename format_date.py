93424c792c28b8cd52772f0ab128dba0b79508a7
DUSMANU Mihai <mihai.dusmanu@yahoo.com>
2016-01-23 19:43:12 +0100

    Fixed a bug in date input.

--- format_date.py
+++ format_date.py
@@ -27,7 +27,7 @@ def mod(year, x):
 
 now = datetime.datetime.now()
 
-s = str(60 - now.second) + "+>" + str(60 - now.minute) + "+>" + str(24 - now.hour) + "+>" + str(nb_days(now.month,now.year) - now.day) + "+>" + \
+s = str(60 - now.second) + "+>" + str(60 - now.minute) + "+>" + str(24 - now.hour) + "+>" + str(nb_days(now.month,now.year) - now.day + 1) + "+>" + \
     str(12 - now.month + 1) + "+>" + str(now.year) + "+>" + str(leap(now.year)) + "+>" + str(mod(now.year, 4)) + "+>" + str(mod(now.year, 100)) + \
     "+>" + str(mod(now.year, 400)) + "+4<"
 print(s)
