b13b4f18789928dbafa0b3f8c51e0eaa64b83f82
Clément Pascutto <clement.pascutto@ens.fr>
2016-01-23 02:43:43 +0100

    Updated current time script.

--- run_clock.sh
+++ run_clock.sh
@@ -5,7 +5,7 @@ min=$((60 - `date +%M`))
 hour=$((24 - `date +%H`))
 
 
-sed "1i\\$sec+>$min+>$hour+40>+" clock/clock_zero.bfpp > clock/clock.bfpp
+sed "1i\\$sec+>$min+>$hour+" clock/clock_zero.bfpp > clock/clock.bfpp
 
 ./create_rom.sh clock/clock.bfpp
 ./sim/netlist_sim.byte -mem proc/rom.mem -steps 0 -net proc/proc.net | python3 sexy_printer.py
